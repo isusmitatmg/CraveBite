2e45bf661a13d62b2c50a3bf30977fd80f01ee06
Susmita Tamang <isusmitatng@gmail.com>
2026-08-11 09:17:39 +0545

    Add landing page

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>CraveBite | Online Food Pre-Order</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<header>
+<nav class="navbar">
+    <div class="brand">
+        CraveBite
+    </div>
+    <ul class="nav-links">
+        <li>
+           <a href="user/menu.php"> Menu</a>     
+       </li>
+
+        <li>
+            <a href="login.php">Login</a>
+        </li>
+
+        <li>
+            <a href="register.php" class="btn">
+                Register
+            </a>
+        </li>
+
+    </ul>
+</nav>
+</header>
+
+<section class="hero">
+<div class="hero-content">
+
+<h1>
+    Your Favorite Food,
+    <span>Just One Click Away</span>
+</h1>
+
+<p>
+    Order delicious meals online and save your waiting time.
+    Fresh food prepared for you.
+</p>
+
+<a href="user/menu.php" class="main-btn">
+    Explore Menu
+</a>
+
+</div>
+<div class="hero-image">
+<img src="uploads/pizza.jpg" 
+alt="Food">
+</div>
+</section>
+
+<section class="popular">
+<h2>Popular Food </h2>
+
+<div class="food-container">
+<div class="food-card">
+<img src="uploads/burger.jpg">
+<h3>Burger</h3>
+<p> Fresh and tasty classic burger </p>
+<button> Order Now </button>
+</div>
+
+<div class="food-card">
+<img src="uploads/pizza.jpg">
+<h3>Pizza</h3>
+<p>Hot cheese pizza</p>
+<button> Order Now </button>
+</div>
+
+<div class="food-card">
+<img src="uploads/pasta.jpg">
+<h3>Pasta</h3>
+<p>Creamy Italian pasta</p>
+<button> Order Now </button>
+</a>
+</div>
+
+</div>
+</section>
+
+<footer>
+<p>© 2026 CraveBite. All Rights Reserved.</p>
+</footer>
+
+</body>
+</html>
